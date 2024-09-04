2af04cf453f9cf203d39b3070630e1c685232223
waldemar10 <waldemar.ju98@gmail.com>
2024-09-04 19:31:05 +0200

    fixed print

--- compressor.py
+++ compressor.py
@@ -130,7 +130,7 @@ def decode(temp_dir, compressed_file, FLAGS, len_series, last):
   torch.manual_seed(FLAGS.random_seed)
 
   model = compress_model.SLiMPerformer(FLAGS.vocab_size, FLAGS.vocab_dim, FLAGS.hidden_dim,FLAGS.n_layers, FLAGS.ffn_dim,FLAGS.n_heads, FLAGS.feature_type, FLAGS.compute_type).cuda()
-  print("torch.cuda.device_count() "+torch.cuda.device_count())
+  print("torch.cuda.device_count() "+ str(torch.cuda.device_count()))
   if torch.cuda.device_count() > 1: 
       print("Multi-GPU Decode")
       model = torch.nn.DataParallel(model) 
@@ -232,7 +232,7 @@ def encode(temp_dir, compressed_file, FLAGS, series, train_data, last_train_data
     model = compress_model.SLiMPerformer(FLAGS.vocab_size, FLAGS.vocab_dim, FLAGS.hidden_dim,
                                              FLAGS.n_layers, FLAGS.ffn_dim,
                                              FLAGS.n_heads, FLAGS.feature_type, FLAGS.compute_type).cuda()
-    print("torch.cuda.device_count() "+torch.cuda.device_count())
+    print("torch.cuda.device_count() "+ str(torch.cuda.device_count()))
     if torch.cuda.device_count() > 1: 
       print("Multi-GPU")
       model = torch.nn.DataParallel(model) 
