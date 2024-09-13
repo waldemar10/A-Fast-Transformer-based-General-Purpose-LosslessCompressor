e20b7aac131edf0718029c95d199eec194b53a29
waldemar10 <waldemar.ju98@gmail.com>
2024-09-13 09:00:54 +0200

    Update compressor.py

--- compressor.py
+++ compressor.py
@@ -280,7 +280,7 @@ def encode(rank,world_size,temp_dir, compressed_file, FLAGS, series, train_data,
 
     # Initialize file handles and encoders for the current GPU's portion of the batch
     start_index = rank * (FLAGS.batch_size // torch.distributed.get_world_size())
-    end_index = ((rank + 1) * (FLAGS.batch_size // torch.distributed.get_world_size()))-1
+    end_index = min((rank + 1) * (FLAGS.batch_size // world_size), len(train_data)) - 1
 
     cpu_usages, memory_usages, gpu_usages = [], [], []
     stop_event = threading.Event()
